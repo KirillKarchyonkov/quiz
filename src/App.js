2567300b1854abac65c5a3125a75a0d84b45aaaa
KirillKarchyonkov <112825082+KirillKarchyonkov@users.noreply.github.com>
2024-10-12 11:16:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ function Result({correct}) {
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
       <h2>Вы отгадали {correct} ответа из 10</h2>
-      <a href='https://kirillkarchyonkov.github.io/quiz/'><button>Попробовать снова</button></a>
+      <a href='/'><button>Попробовать снова</button></a>
     </div>
   );
 }
