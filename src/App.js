635159c527c2f5a7a0200eb970f0cbb4ce84754d
Kirill Karchyonkov <kirya.karchenkov@gmail.com>
2024-10-12 11:32:14 +0300

    update reload

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ function Result({correct}) {
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
       <h2>Вы отгадали {correct} ответа из 10</h2>
-      <a href='/'><button>Попробовать снова</button></a>
+      <button onClick={() => window.location.reload()}>Попробовать снова</button>
     </div>
   );
 }
